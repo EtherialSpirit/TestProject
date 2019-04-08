db20f7e383b9433fba3cd56b56347f7336cab97b
Artem <etherialspirit@gmail.com>
2019-04-08 09:58:57 +0200

    add main.go

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	_ "io"
+	"log"
+	"os"
+	_ "os"
+	"strings"
+)
+
+type siteRoad struct{
+	status bool
+	url string
+}
+
+func linkScrape(url1 string)  string{
+
+	doc, err := goquery.NewDocument(url1)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var link1 string
+
+	doc.Find("body a").Each(func(index int, item *goquery.Selection) {
+		linkTag := item
+		link, _ := linkTag.Attr("href")
+		linkText := linkTag.Text()
+
+		//fmt.Printf("Link #%d: '%s' - '%s'\n", index, linkText, link)
+		//if withoutSpaces(strings.ToUpper(linkText)) == "КОНТАКТЫ" || withoutSpaces(strings.ToUpper(linkText))  == "CONTACT" ||withoutSpaces(strings.ToUpper(linkText)) == "KONTAKT" {
+		if (strings.Contains(strings.ToUpper(linkText), "КОНТАКТ") && !strings.Contains(strings.ToUpper(linkText), "ВКОНТАКТ"))|| strings.Contains(strings.ToUpper(linkText), "CONTACT")	{//link1 = cut(link, 4)
+
+			link1 = condition(link, url1);
+
+		}else if strings.Contains(strings.ToUpper(link), "CONTACT")||strings.Contains(strings.ToUpper(link), "КОНТАКТ")||strings.Contains(strings.ToUpper(link), "KONTAKT"){
+
+			link1 = condition(link, url1);
+		}
+
+		//if strings.Contains(strings.ToUpper(link), "CONTACT"){
+		//
+		//	link1 = condition(link, url1);
+		//}
+		})
+
+	return link1
+
+}
+
+func main() {
+
+	file, err := os.Open("Site.txt")
+	if err != nil {
+		// здесь перехватывается ошибка
+		return
+	}
+	defer file.Close()
+
+	// получить размер файла
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
+	// чтение файла
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return
+	}
+
+	str := string(bs)
+	s := strings.Split(str, "\r\n")
+
+	var siteRoad1 = siteRoad{url: "", status: true}
+
+	for ii := 0; ii < len(s); ii++ {
+		rEmpty  := linkScrape(s[ii])
+		if rEmpty != ""{
+			siteRoad1.url = rEmpty
+			siteRoad1.status = true
+			fmt.Println(siteRoad1)
+		}else{
+			fmt.Println("false")
+		}
+	}
+	}
+
+func condition(link string, url1 string) string{
+
+	if cut(link, 4) == "http" {
+		return  link
+	}else {
+		link = url1 + link
+		return  link
+	}
+
+}
+
+func cut(text string, limit int) string {
+	runes := []rune(text)
+	if len(runes) >= limit {
+		return string(runes[:limit])
+	}
+	return text
+}
+
+
